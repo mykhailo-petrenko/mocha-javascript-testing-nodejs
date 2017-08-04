d95d9a27d444e3902459d0946b068a055a48dc63
Mykhailo Petrenko <mihal@seven-bytes.com>
2017-08-04 13:37:09 +0300

    Testing Objects

---
+++ test/objects.spec.js
@@ -0,0 +1,24 @@
+describe.only("Objects demo", function() {
+
+    var user = {
+        name: "John",
+        gender: "male",
+    };
+
+    var anotherUser = {
+        name: "John",
+        gender: "male"
+    };
+
+    it("should be deal with objects", function() {
+        user.should.have.property("gender");
+    });
+
+    it("should have name John", function() {
+        user.should.have.property("name").and.equal("John");
+    });
+
+    it("Should compare objects", function() {
+        user.should.be.deep.equal(anotherUser);
+    });
+});
